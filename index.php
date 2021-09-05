84a195d858c0cabb47ad8d30ad4b1d909161a359
tsalmeida <tsilvaalmeida@gmail.com>
2021-09-05 13:16:43 -0300

    Changes

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <!doctype html>
 <html lang=\"en\">
-  <head>
+<head>
     <!-- Required meta tags -->
     <meta charset=\"utf-8\">
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
     <!-- Bootstrap CSS -->
-    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We\" crossorigin=\"anonymous\">
-      <link href='stylesheets/main.css?20200812' rel='stylesheet'>
+    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css\" rel=\"stylesheet\"
+          integrity=\"sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We\"
+          crossorigin=\"anonymous\">
+    <link href='stylesheets/main.css?20210905' rel='stylesheet'>
     <title>Nexustation</title>
-  </head>
-  <body>
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj\" crossorigin=\"anonymous\"></script>
-
-  </body>
+</head>
+<body>
+<h1 class="fontstack-mono">Nexustation</h1>
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+</body>
 </html>
